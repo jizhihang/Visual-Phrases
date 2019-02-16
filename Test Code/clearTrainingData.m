814628b0b1cc2e1cf9d1b33d3f077f77bb628b35
Brian_Desk <bkim129@gmail.com>
2019-02-16 18:37:00 -0500

    removed misc files

--- Test Code/clearTrainingData.m
+++
@@ -1,6 +0,0 @@
-% Clear Training input and output to make testing easier to see.
-
-TrainingInput = {};
-TrainingOutput = {};
-save('TrainingOutputA.mat', 'TrainingOutput');
-save('TrainingInputA.mat', 'TrainingInput');
